9453d1f33daffb50e17406fed3ae781240ecefae
Your Name <you@example.com>
2021-12-18 12:24:18 +0330

    added search component to project

--- src/components/TransActionsComponent.js
+++ src/components/TransActionsComponent.js
@@ -1,17 +1,54 @@
-const TransActionsComponent = ({ transactions }) => {
+import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+
+const TransActionsComponent = (props) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredTnx, setFilteredTnx] = useState(props.transactions);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilteredTnx(props.transactions);
+      return;
+    }
+    const filtered = props.transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTnx(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [props.transactions]);
+
+  if (!props.transactions.length) {
+    return <h3>add some transaction</h3>;
+  }
   return (
     <section>
-      {transactions.length &&
-        transactions.map((t) => (
-          <div
-            key={t.id}
-            className="transaction"
-            style={{ borderRight: t.type === "expense" && "4px solid red" }}
-          >
-            <span>{t.desc}</span>
-            <span>$ {t.amount}</span>
-          </div>
-        ))}
+      <input
+        type="text"
+        value={searchItem}
+        onChange={changeHandler}
+        placeholder="search for tnx..."
+        className="search"
+      />
+      {filteredTnx.length
+        ? filteredTnx.map((t) => (
+            <div
+              key={t.id}
+              className="transaction"
+              style={{ borderRight: t.type === "expense" && "4px solid red" }}
+            >
+              <span>{t.desc}</span>
+              <span>$ {t.amount}</span>
+            </div>
+          ))
+        : "no item matchs!"}
     </section>
   );
 };
